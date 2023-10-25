dcabac27a93d85bc40893106340cdf9f5e4df991
Sergey Poprotsky <s.poprotsky@mbp-sergey.home>
2023-10-25 16:22:44 +0200

    Update dating.js

--- dating.js
+++ dating.js
@@ -12,8 +12,8 @@ config();
 
 const CREO_PATH = `${process.env.FTP_PATH_TO_CREOS}/dating/creo`;
 const END_PATH = `${process.env.FTP_PATH_TO_CREOS}/dating/end`;
-// const command = (creoPath, endPath, outputName) => `ffmpeg -i "${creoPath}" -i "${endPath}" -filter_complex "[0:v][0:a][1:v][1:a]concat=n=2:v=1:a=1[outv][outa]" -map "[outv]" -map "[outa]" -c:v libx264 -c:a aac "${outputName}"`;
-const command = (creoPath, endPath, outputName) => `ffmpeg -i "${creoPath}" -i "${endPath}" -filter_complex "[0:v]scale=464:848[v0];[1:v]scale=464:848[v1];[v0][0:a][v1][1:a]concat=n=2:v=1:a=1[outv][outa]" -map "[outv]" -map "[outa]" -c:v libx264 -c:a aac "${outputName}"`;
+const command = (creoPath, endPath, outputName) => `ffmpeg -i "${creoPath}" -i "${endPath}" -filter_complex "[0:v]scale=464:848,setsar=1[v0];[1:v]scale=464:848,setsar=1[v1];[v0][0:a][v1][1:a]concat=n=2:v=1:a=1[outv][outa]" -map "[outv]" -map "[outa]" -c:v libx264 -c:a aac "${outputName}"`;
+// const command = (creoPath, endPath, outputName) => `ffmpeg -i "${creoPath}" -i "${endPath}" -filter_complex "[0:v]scale=464:848[v0];[1:v]scale=464:848[v1];[v0][0:a][v1][1:a]concat=n=2:v=1:a=1[outv][outa]" -map "[outv]" -map "[outa]" -c:v libx264 -c:a aac "${outputName}"`;
 
 const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
 
@@ -45,8 +45,9 @@ bot.on('message', async msg => {
                     await getVideo(outputEndPath, inputEndPath);
                     await fs.readFile(outputEndPath, { encoding: 'utf-8' }, () => {});
                 }
-
-                const creoName = data.filter(el => el.name.endsWith('.mp4')).map(el => el.name)[i];
+                const creos = data;
+                creos.length = Number(process.env.VIDEOS_COUNT);
+                const creoName = creos.filter(el => el.name.endsWith('.mp4')).map(el => el.name)[i];
                 const inputCreoPath = `${CREO_PATH}/${creoName}`;
                 const outputCreoName = `./${getRandomItem(rndArray)}_${randInt()}.mp4`;
                 await getVideo(outputCreoName, inputCreoPath);
